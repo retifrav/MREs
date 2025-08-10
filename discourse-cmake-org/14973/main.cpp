abecf111e3551c6c425a76d11f1eea6113b37f84
retif <retif@decovar.dev>
2025-08-10 14:07:27 +0200

    14973: Fixed some issues

--- discourse-cmake-org/14973/main.cpp
+++ discourse-cmake-org/14973/main.cpp
@@ -3,34 +3,23 @@
 
 #include "R3DSDK.h"
 
-using namespace R3DSDK;
-
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    // Set up code that uses the Qt event loop here.
-    // Call a.quit() or a.exit() to quit the application.
-    // A not very useful example would be including
-    // #include <QTimer>
-    // near the top of the file and calling
-    // QTimer::singleShot(5000, &a, &QCoreApplication::quit);
-    // which quits the application after 5 seconds.
+    // Initialize the R3DSDK prior to using any R3DSDK objects
+    auto status = R3DSDK::InitializeSdk(".", OPTION_RED_NONE);
 
-    // If you do not need a running Qt event loop, remove the call
-    // to a.exec() or	// Initialize the R3DSDK prior to using any R3DSDK objects.
-    InitializeStatus status = InitializeSdk(".", OPTION_RED_NONE);
+    if ( status != R3DSDK::ISInitializeOK)
+    {
+        printf("Failed to initialize SDK: %d\n", status);
+        return 1;
+    }
 
-//    if ( status != ISInitializeOK)
-//    {
-//        printf("Failed to initialize SDK: %d\n", status);
-//        return 1;
-//    }
-
-//    printf("SDK VERSION: %s\n",  GetSdkVersion());
+    printf("SDK VERSION: %s\n", R3DSDK::GetSdkVersion());
 
     // Finalize the R3DSDK after destroying all R3DSDK objects
-//    FinalizeSdk();
+    R3DSDK::FinalizeSdk();
 
     qDebug() << "Qt - Link to R3DSDK.";
     return a.exec();
